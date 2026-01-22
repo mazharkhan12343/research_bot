03acc97a6e80138e408004c76b1fda77e863df35
Mazhar Manzoor <105038699+mazharkhan12343@users.noreply.github.com>
2026-01-22 16:45:05 +0900

    Add files via upload

--- bot.py
+++ bot.py
@@ -46,4 +46,7 @@ async def main():
 
 if __name__ == "__main__":
     import asyncio
-    asyncio.run(main())
+    loop = asyncio.get_event_loop()
+    loop.create_task(main())
+    loop.run_forever()
+
